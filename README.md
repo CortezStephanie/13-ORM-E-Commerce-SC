b6c7068be0b06e044b8503684ed7eb345261c34d
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-11-09 23:10:32 -0600

    finished readme and video

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # 13-ORM-E-Commerce-SC
+
+
+#  Ecommerce
+![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+##  Using RESTful API's with CRUD functionality we created a backend E-commerce. Because this is only backend we used Insomnia to test the API's.
+### Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributors](#contributors)
+- [License](#license)
+### Installation
+in terminal npm i then after run your schema. Workbench was used here but command line also works. After running schema then npm run seed. Finally we start the server by using node index.js in the command line. 
+### Usage
+Once server is started use the local host "http://localhost:3001/api" and add on to the url as needed to test. 
+## License
+This repository is covered by the MIT License  <br> 
+https://opensource.org/licenses/MIT
+#### Contributors
+Ethan Cho, Damien Luzzo, and Chuck Stephens
+#### Test Instructions
+Insomnia
+##### Questions
+If you have any questions please feel free to contact me at cortezstephanie333@yahoo.com <br>
+GitHub: CortezStephanie 
+##### Links
+Please [click here](https://watch.screencastify.com/v/yhGIO1QgV7qzWFXEpKvN) to watch video of the testing for reference.
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }]
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Product data
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagUpdate = await Tag.update(
+      {
+      tag_name: req.body.tag_name,
+    }, 
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagUpdate[0]) {
+      res.status(404).json({ message: 'no tag with this id' });
+      return;
+    }
+    res.status(200).json(tagUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagDel = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagDel) {
+      res.status(404).json({ message: 'No tag with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagDel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
